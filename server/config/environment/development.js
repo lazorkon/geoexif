9664a3c9fa34abccefb871689db098eb8ecd2717
Nazar Lazorko <lazorkon@gmail.com>
2014-07-30 19:49:49 +0300

    initial commit

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/geoexif-dev'
+  },
+
+  seedDB: true
+};
