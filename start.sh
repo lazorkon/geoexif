36ba03c6161392e385783927cd566d6e5dc566a5
Nazar Lazorko <lazorkon@gmail.com>
2014-10-07 19:35:18 +0300

    fixed start/stop scripts

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # node can not start on port 80 because lower port numbers require root privileges
 # solution: edit /etc/rc.local or /ect/rc.d/local and add following command without sudo
@@ -12,14 +12,16 @@
 if [ $(ps aux | grep $USER | grep node | grep -v grep | wc -l | tr -s "\n") -eq 0 ]; then
     export PORT=8080
     export NODE_ENV=production
+    SCRIPT_PATH=$(dirname $0)
     DATE=$(date +%Y-%m-%d_%H%M)
     LOG="./log/$DATE-forever.log"
     OUT_LOG="./log/$DATE-out.log"
     ERR_LOG="./log/$DATE-err.log"
+    echo $SCRIPT_PATH
     if cd $SCRIPT_PATH ; then
         if [[ ! -d log ]] ; then 
             mkdir log
         fi
-        ./node_modules/.bin/forever --uid 'tagmazing' -m 5 --plain --no-colors --minUptime 1000 --spinSleepTime 10000 -p $(pwd) -l $LOG -o $OUT_LOG -e $ERR_LOG start server/app.js
+        ./node_modules/forever/bin/forever --uid 'tagmazing' -m 5 --plain --no-colors --minUptime 1000 --spinSleepTime 10000 -p $(pwd) -l $LOG -o $OUT_LOG -e $ERR_LOG start server/app.js
     fi
 fi
--- stop.sh
+++ stop.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-cd $(dirname $0) && ./node_modules/.bin/forever list && ./node_modules/.bin/forever stop 'tagmazing'
+cd $(dirname $0) && ./node_modules/forever/bin/forever list && ./node_modules/forever/bin/forever stop 'tagmazing'
